1deda5d6da079f10b082dc68bf921a9b4b826911
JezusCC <2812903195@qq.com>
2020-11-10 20:12:17 +0800

    幻灯片组件

---
+++ src/components/carousel/carousel.vue
@@ -0,0 +1,65 @@
+<template>
+	<div class="carouselbox">
+		<div class="carousel-left"></div>
+		<div class="carousle-center">
+			<div @mouseenter="stopSwitch()" @mouseleave="startSwitch()"
+			class="carousel-items">
+				<ul>
+					<li v-for="item in carouselArr">
+						<img :src="item.imgurl" alt="">
+					</li>
+					<li>
+						<img :src="carouselArr[0].imgurl" alt="">
+					</li>
+				</ul>
+			</div>
+			<!-- 幻灯片下面的点点 -->
+			<div class="carousle-index">
+				<ul>
+					<li v-for="(item,index) in getCarouselNums" @click="switchToIndex(index)">
+						<span :class="{'carousel-idx-active':index == carouselIdx}"></span>
+					</li>
+				</ul>
+			</div>
+		</div>
+		<div class="carousle-right"></div>
+	</div>
+</template>
+
+<script>
+	export default {
+		name:'Carousel',
+		data(){
+			return {
+				carouselArr:[
+					{imgurl:'aaa',jmpurl:'bbb'},
+					{imgurl:'aaa',jmpurl:'bbb'},
+					{imgurl:'aaa',jmpurl:'bbb'},
+					{imgurl:'aaa',jmpurl:'bbb'}
+				],
+				carouselIdx:0,
+				carouselAuto:true
+			}
+		},
+		methods:{
+			startSwitch(){
+				this.carouselAuto = true
+			},
+			stopSwitch(){
+				this.carouselAuto = false
+			},
+			switchToIndex(index){
+				this.carouselIdx = index
+			}
+		},
+		computed:{
+			getCarouselNums(){
+				return this.carouselArr.length
+			}
+		}
+	}
+</script>
+
+<style>
+	@import url("../../assets/css/carousel/carousel.css");
+</style>
