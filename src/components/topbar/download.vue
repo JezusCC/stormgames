0c73a56afb9e385a0305b911772010e85c3c17f1
JezusCC <2812903195@qq.com>
2020-11-10 09:25:07 +0800

    download.vue 页面

---
+++ src/components/topbar/download.vue
@@ -0,0 +1,32 @@
+<template>
+	<div>
+		<div>
+			<h3>下载基于Windows平台的Storm客户端</h3>
+			<ul>
+				<li>Storm for Windows 32 Bit</li>
+				<li>Storm for Windows 64 Bit</li>
+				<li>Storm for Windows Fullstack Beta </li>
+			</ul>
+		</div>
+		<div>
+			<h3>下载基于Linux平台的Storm客户端</h3>
+			<ul>
+				<li>Storm for Linux</li>
+				<li>Storm for Linux Fullstack Beta </li>
+			</ul>
+		</div>
+		<div>
+			<h3>下载基于Mac平台的Storm客户端</h3>
+			<ul>
+				<li>Storm for Mac</li>
+				<li>Storm for Mac Fullstack Beta</li>
+			</ul>
+		</div>
+	</div>
+</template>
+
+<script>
+</script>
+
+<style>
+</style>
