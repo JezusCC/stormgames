ca9b9637d211c874999fc9dcc51b96455d4566cc
JIAODAHAN <1272238987@qq.com>
2020-11-12 15:53:55 +0800

    填充客服模块内容

--- src/views/shopserver/Shopsever.vue
+++ src/views/shopserver/Shopsever.vue
@@ -1,23 +1,41 @@
 <template>
-	<div>
-		<div>
-			<ul>
-				<li v-for="item in serverOption" 
-					@mouseenter="optionAnimate()"
-					@click="showDetailOption()">
-				{{item}}
-				</li>
-			</ul>                                                                                                                               
+	<div class="max">
+		<div class="changebox">
+			<div class="server_left">
+				<ul>
+					<li v-for="(item,index) in serverOption" :style="generateRandomColor()"
+						@click="showDetailOption(index)">
+					{{item}}
+					</li>
+				</ul>
+			</div>
+			<div class="server_right"  @mouseout="resetClickItem()">
+				<ul  v-if="clickItem == 1000">
+					<li v-for="item in more" :style="generateRandomColor()">
+					{{item}}
+					</li>
+				</ul>
+				<form action="" v-else-if="clickItem == 0">
+					
+				</form>
+				<form action="" v-else-if="clickItem == 1">
+					123456
+				</form>
+				<form action="" v-else-if="clickItem == 2">
+					123456
+				</form>
+				<form action="" v-else-if="clickItem == 3">
+					123456
+				</form>
+			</div>
 		</div>
-		<div>
-			<ul>
-				<li ></li>
-			</ul>
-		</div>
+		<button>人工客服</button>
 	</div>
+	
 </template>
 
 <script>
+	import cardbox from '../../components/cardbox/cardbox.vue'
 	export default {
 		data(){
 			return{
@@ -26,15 +44,29 @@
 					'商品问题',
 					'账户问题',
 					'开发者疑难'
-				]
+				],
+				more:[
+					'游戏未添加到我库中，游戏序列号出错等',
+					'退款，DLC或额外内容缺少等',
+					'充值未到账，我的扣除金额有误等',
+					'stormgames硬件帮助，客户端问题等'
+				],
+				clickItem:1000
 			}
 		},
 		methods:{
-			optionAnimate(){
-				
+			generateRandomColor(){
+				//rgba
+				let r = Math.floor(Math.random() * (255 - 0)) + 0
+				let g = Math.floor(Math.random() * (255 - 0)) + 0
+				let b = Math.floor(Math.random() * (255 - 0)) + 0
+				return 'background-color:rgba('+r+','+g+','+b+','+1+');'
 			},
-			showDetailOption(){
-				
+			showDetailOption(idx){
+				this.clickItem = idx
+			},
+			resetClickItem(){
+				this.clickItem = 1000
 			}
 		}
 	}
---
+++ src/views/shopserver/shopsever.md
@@ -0,0 +1,9 @@
+v-bind,   	:
+v-on,		@
+v-if 	"表达式"
+v-for, "变量名 in 数组"		{{变量}}
+		"(变量名,索引) in 数组"
+
+created(){}
+
+components:{ 键值对 }
