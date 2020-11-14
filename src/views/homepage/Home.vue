01fc478ba17d05aa607fd6e22b2e0e9a580cf06f
JezusCC <2812903195@qq.com>
2020-11-14 16:13:20 +0800

    V1.0

--- src/views/homepage/Home.vue
+++ src/views/homepage/Home.vue
@@ -25,13 +25,13 @@
 			<!-- 上部轮播图-->
 			<carousel></carousel>
 			<!-- 卡片容器，容器内可盛放4张卡片：近期最热 -->
-			<cardbox :items="getItems('hot')" name="近期热门"></cardbox>
+			<cardbox :items="hotProduct" name="近期热门"></cardbox>
 			<!-- 卡片容器，容器内可盛放4张卡片：商城上新 -->
-			<cardbox :items="getItems('new')" name="商城上新"></cardbox>
+			<cardbox :items="newProduct" name="商城上新"></cardbox>
 			<!-- 卡片容器，容器内可盛放4张卡片：特惠商品 -->
-			<cardbox :items="getItems('count')" name="特惠商品"></cardbox>
+			<cardbox :items="countProduct" name="特惠商品"></cardbox>
 			<!-- 卡片容器，容器内可盛放4张卡片：免费商品 -->
-			<cardbox :items="getItems('free')" name="免费商品"></cardbox>
+			<cardbox :items="freeProduct" name="免费商品"></cardbox>
 		
 		</div>
 	</div>
@@ -45,49 +45,56 @@
 			cardbox,
 			carousel
 		},
+		created:function(){
+			this.getItems('hot')
+			this.getItems('new')
+			this.getItems('count')
+			this.getItems('free')
+		},
 		data(){
 			return {
 				categoryList:[
 					{title:'推荐商品',item:['高分评价推荐','高销量商品']},
 					{title:'按类型筛选',item:['角色扮演','开放世界','第一人称','模拟经营','策略竞速','动作冒险']},
 					{title:'平台',item:['PC','游戏主机','移动端','虚拟现实','storm云']}
-				]
+				],
+				hotProduct:[],
+				newProduct:[],
+				countProduct:[],
+				freeProduct:[]
 			}
 		},
 		methods:{
 			getItems(type){
 				if(type === 'hot'){
 					//请求
-					return [
-						{imgurl:'aaa',jmpurl:'http://121.196.110.115:4000/test'},
-						{imgurl:'aaa',jmpurl:'https://www.baidu.com'},
-						{imgurl:'aaa',jmpurl:'bbb'},
-						{imgurl:'aaa',jmpurl:'bbb'}
-					]
+					console.log(this.$baseip+'/product_hot')
+					this.$axios.get(this.$baseip+'/product-hot').then((items)=>{
+						this.hotProduct = items.data.hotproduct
+					},(err)=>{
+						console.log(err)
+					})
 				}else if(type === 'new'){
 					//请求
-					return [
-						{imgurl:'aaa',jmpurl:'#'},
-						{imgurl:'aaa',jmpurl:'#'},
-						{imgurl:'aaa',jmpurl:'#'},
-						{imgurl:'aaa',jmpurl:'#'}
-					]
+					this.$axios.get(this.$baseip+'/product-new').then((items)=>{
+						this.newProduct = items.data.newproduct
+					},(err)=>{
+						console.log(err)
+					})
 				}else if(type === 'count'){
 					//请求
-					return [
-						{imgurl:'aaa',jmpurl:'#'},
-						{imgurl:'aaa',jmpurl:'#'},
-						{imgurl:'aaa',jmpurl:'#'},
-						{imgurl:'aaa',jmpurl:'#'}
-					]
+					this.$axios.get(this.$baseip+'/product-count').then((items)=>{
+						this.countProduct = items.data.countproduct
+					},(err)=>{
+						console.log(err)
+					})
 				}else if(type === 'free'){
 					//请求
-					return [
-						{imgurl:'aaa',jmpurl:'#'},
-						{imgurl:'aaa',jmpurl:'#'},
-						{imgurl:'aaa',jmpurl:'#'},
-						{imgurl:'aaa',jmpurl:'#'}
-					]
+					this.$axios.get(this.$baseip+'/product-free').then((items)=>{
+						this.freeProduct = items.data.freeproduct
+					},(err)=>{
+						console.log(err)
+					})
 				}
 			},
 			jumpToSearch(){
